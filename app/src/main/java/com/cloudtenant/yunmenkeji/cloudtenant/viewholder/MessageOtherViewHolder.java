8cd75eb3b0b5403bdc8fbc91fc6b74c3a1848363
TLOL20\tlol2 <353355472@qq.com>
2018-06-20 22:32:50 +0800

    添加消息页面

--- app/src/main/java/com/cloudtenant/yunmenkeji/cloudtenant/viewholder/MessageOtherViewHolder.java
+++ app/src/main/java/com/cloudtenant/yunmenkeji/cloudtenant/viewholder/MessageOtherViewHolder.java
@@ -5,7 +5,7 @@ import android.widget.TextView;
 
 import com.cloudtenant.yunmenkeji.cloudtenant.R;
 import com.cloudtenant.yunmenkeji.cloudtenant.bean.Contract;
-import com.cloudtenant.yunmenkeji.cloudtenant.bean.MyFamily;
+import com.cloudtenant.yunmenkeji.cloudtenant.bean.MessageOther;
 import com.jude.easyrecyclerview.adapter.BaseViewHolder;
 
 import de.hdodenhof.circleimageview.CircleImageView;
@@ -14,22 +14,22 @@ import de.hdodenhof.circleimageview.CircleImageView;
 /**
  * Created by Mr.Jude on 2015/2/22
  */
-public class ContractViewHolder extends BaseViewHolder<Contract> {
+public class MessageOtherViewHolder extends BaseViewHolder<MessageOther> {
     private TextView name;
-    private TextView fans;
-    private TextView note;
     private TextView time;
-    private CircleImageView icon;
 
-    public ContractViewHolder(ViewGroup parent) {
-        super(parent, R.layout.item_my_contract);
-        name = $(R.id.name);
+
+    public MessageOtherViewHolder(ViewGroup parent) {
+        super(parent, R.layout.item_message_other);
+        name = $(R.id.tv_message);
+        time = $(R.id.tv_time);
 
     }
 
     @Override
-    public void setData(final Contract person){
+    public void setData(final MessageOther person){
         name.setText(person.getFamily());
+        time.setText(person.getTime());
 
         //name.setText(person.getName());
     }
